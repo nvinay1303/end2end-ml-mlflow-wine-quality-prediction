2aaae0e2d641cb00244053cf17794a502b2b46a8
Nikhil Vinay <n.vinay@gridx.de>
2025-03-27 20:41:11 +0100

    adds data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.ml_project import logger
 from src.ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,3 +12,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
