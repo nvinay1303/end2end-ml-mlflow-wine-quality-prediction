556d951e31a72fb9c30a1efd26acad6c0f11ddf8
Nikhil Vinay <n.vinay@gridx.de>
2025-03-31 00:25:02 +0200

    adds model trainer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.ml_project import logger
 from src.ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.ml_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.ml_project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -34,3 +35,12 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
