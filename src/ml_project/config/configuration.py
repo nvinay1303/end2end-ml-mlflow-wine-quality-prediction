7667b3986ba1b7d048082a705487070bf9d711aa
Nikhil Vinay <n.vinay@gridx.de>
2025-04-05 15:56:47 +0200

    model evaluation updates

--- src/ml_project/config/configuration.py
+++ src/ml_project/config/configuration.py
@@ -92,7 +92,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
+            mlflow_uri="https://dagshub.com/nvinay1303/end2end-ml-mlflow-wine-quality-prediction.mlflow",
            
         )
 
